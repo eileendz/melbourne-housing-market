94f4e419295b03e58586bd0f1ad80c103f09b910
Huizezhangsherry <36781502+huizezhang-sherry@users.noreply.github.com>
2019-10-04 13:14:53 +1000

    some notes

--- house.R
+++ house.R
@@ -3,24 +3,25 @@ library(tidyverse)
 
 # load data & factorisation
 house_less_raw<- read_csv("data/MELBOURNE_HOUSE_PRICES_LESS.csv") %>% 
-  mutate(Suburb = as.factor(Suburb), 
-         Address = as.factor(Address),
-         Type = as.factor(Type), 
-         Method = as.factor(Method), 
-         Date = as.Date(Date,"%d/%m/%Y"),
-         Postcode = as.factor(Postcode), 
-         Regionname = as.factor(Regionname),
-         CouncilArea = as.factor(CouncilArea)) 
+  mutate(suburb = as.factor(Suburb), 
+         address = as.factor(Address),
+         type = as.factor(Type), 
+         method = as.factor(Method), 
+         date = as.Date(Date,"%d/%m/%Y"),
+         postcode = as.factor(Postcode), 
+         regionname = as.factor(Regionname),
+         councilarea = as.factor(CouncilArea), 
+         price = Price/ 1e3)
 
 # drop missing house price, assign category (1-10) to price based on quantile. 
 # Then our problem can be re-phrased into: predict which category will the price 
 # falls into, which is a classification problem!
 house_less<- house_less_raw %>% 
-  filter(!is.na(Price)) %>% 
-  mutate(Price_class = findInterval(Price, quantile(Price, probs=0:10/10)))
+  filter(!is.na(price)) %>% 
+  mutate(Price_class = findInterval(price, quantile(price, probs=0:10/10)))
 
 # no missings!
-naniar::vis_miss(house_less)
+naniar::vis_miss(house_less, warn_large_data = FALSE)
 
 # For the nn, we could do something like "self-organizing map (SOM)" or "Learning vector quantization" (LVQ), 
 # which is popular for multi-label classification. Multi-Task Learning may also
@@ -29,6 +30,8 @@ naniar::vis_miss(house_less)
 # https://link.springer.com/content/pdf/10.1023%2FA%3A1023977111302.pdf
 # https://arxiv.org/pdf/1901.01774.pdf
 
-  
+library(kohonen)
+som_grid <- somgrid(xdim = 20, ydim=20, topo="hexagonal")
+
 
 
